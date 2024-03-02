6792a5e3d52fb2f795eb477665e47bdd5ac405b9
Shelby Jueden <shelby@jueden.org>
2024-03-02 08:25:25 -0700

    Standardize to gfx extension for bitmap data

--- projects/example-project/gfx/convert.sh
+++ projects/example-project/gfx/convert.sh
@@ -5,9 +5,9 @@ echo "" > gfx.c
 
 for file in *.tga
 do
-	gfx2gba -q -t8 -c16 -x -St.tga.raw -D -P "$file"
+	gfx2gba -q -t8 -c16 -x -St.tga.gfx -D -P "$file"
 	gfx2gba -q -t8 -c16 -x -Sp.tga.pal -D -G "$file"
 
-	b2x -c -n "${file%.*}_gfx" -t u8 < "$file.raw" >> "gfx.c"
+	b2x -c -n "${file%.*}_gfx" -t u8 < "$file.gfx" >> "gfx.c"
 	b2x -c -n "${file%.*}_pal" -t u8 < "$file.pal" >> "gfx.c"
 done
