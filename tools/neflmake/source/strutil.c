2ec6250769d5acf6809255ec714e9095a8227eea
Shelby Jueden <shelby@jueden.org>
2024-03-09 09:13:13 -0700

    Update projects and neflmake source for building on modern systems. Makes entire workflow 100% linux native.

--- tools/neflmake/source/strutil.c
+++ tools/neflmake/source/strutil.c
@@ -18,3 +18,15 @@ void extract_file_name( s8* datadst, s8* datasrc)
 		strcpy( datadst, datasrc);
 	}
 }
+
+
+int stricmp(const char *a, const char *b) {
+  int ca, cb;
+  do {
+     ca = (unsigned char) *a++;
+     cb = (unsigned char) *b++;
+     ca = tolower(toupper(ca));
+     cb = tolower(toupper(cb));
+   } while (ca == cb && ca != '\0');
+   return ca - cb;
+}
--- tools/neflmake/source/strutil.h
+++ tools/neflmake/source/strutil.h
@@ -2,7 +2,9 @@
 #define STRUTIL_H
 
 #include "def.h"
+#include <ctype.h>
 
 void extract_file_name( s8* datadst, s8* datasrc);
+int stricmp(const char *a, const char *b);
 
 #endif
